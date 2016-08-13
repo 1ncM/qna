8d316ee2059530b5012e8708a99b8644561b1418
Alexandr <microposts@gmail.com>
2016-08-13 17:31:48 +0300

    added test for validates question_id

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question, class_name: 'Question', foreign_key: :question_id
 
-  validates :owner, :email, :body, presence: true
+  validates :owner, :email, :body, :question_id, presence: true
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :owner }
   it { should validate_presence_of :email }
   it { should validate_presence_of :body }
+  it { should validate_presence_of :question_id }
   it { should belong_to :question }
 end
