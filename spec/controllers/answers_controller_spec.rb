b613c192431db054f531a7d868459a7afccc8962
Alexandr <microposts@gmail.com>
2016-08-22 23:19:12 +0300

    added testing AnswerController

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question)}
+  let(:answer) { create(:answer) }
+
+  describe 'POST #create' do
+
+    context 'with valid answer' do
+      it 'save answer in database' do
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question }}.to change(Answer,:count).by(1)
+      end
+
+      it 'redirect_to @question' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid answer' do
+      it 'not save in database' do
+        expect { post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }}.to_not change(Answer,:count)        
+      end
+
+      it 'render new view' do
+        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question}        
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
