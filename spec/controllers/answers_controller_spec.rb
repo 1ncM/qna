33e4d3da3dadfe83509d68ee9676b82e32e1877a
Alexandr <microposts@gmail.com>
2016-08-24 21:35:07 +0300

    corrected comments

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with valid answer' do
       it 'save answer in database' do
-        expect { post :create, params: { answer: attributes_for(:answer), question_id: question }}.to change(Answer,:count).by(1)
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question }}.to change(question.answers,:count).by(1)
       end
 
       it 'redirect_to @question' do
@@ -19,7 +19,7 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with invalid answer' do
       it 'not save in database' do
-        expect { post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }}.to_not change(Answer,:count)        
+        expect { post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }}.to_not change(question.answers,:count)        
       end
 
       it 'render new view' do
