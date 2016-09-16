f4d997b6020e5552abfcc331fc5b13c59722f6c5
Alexandr <microposts@gmail.com>
2016-09-16 17:17:43 +0300

    added green spec answer_for_question

---
+++ spec/acceptance/answer_for_question.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'write answer for question', %{
+  In order to help solve the problem
+  As an authenticated user
+  I want to be able to write answer for question
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  scenario 'Authenticated user write answer for question' do
+    sign_in(user)
+    visit question_path(question)
+    fill_in 'answer_body', with: 'Text text'
+    click_on 'Post Your Answer'
+    expect(page).to have_content('Text text')
+  end
+
+end
