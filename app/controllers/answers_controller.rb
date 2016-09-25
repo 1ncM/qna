d819a4ff14eafd38543107b196e640f6d497fee3
Alexandr <microposts@gmail.com>
2016-09-25 18:03:53 +0300

    added answer_delete_spec

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,11 @@
 class AnswersController < ApplicationController
-  before_action :set_question
+  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :set_question, only: [:create, :destroy]
+  before_action :find_answer, only: [:destroy]
+  before_action :must_be_author!, only: [:destroy]
 
   def create
-    @answer = @question.answers.new(answer_params)
+    @answer = @question.answers.new(answer_params.merge(user: current_user))
     if @answer.save
       redirect_to @question
     else
@@ -10,7 +13,18 @@ class AnswersController < ApplicationController
     end
   end
 
+  def destroy
+    @answer.destroy
+    redirect_to @question
+  end
+
   private
+
+  def must_be_author!
+    unless current_user.author_of?(@answer)
+      redirect_to @question, error: "You can delete only your answer"
+    end
+  end
 
   def set_question
     @question = Question.find(params[:question_id])  
@@ -18,5 +32,9 @@ class AnswersController < ApplicationController
 
   def answer_params
     params.require(:answer).permit(:body)
+  end
+
+  def find_answer
+    @answer = @question.answers.find(params[:id])
   end
 end
