79313a12bb6af54fe716c8b99e727761a2a9ed89
Alexandr <microposts@gmail.com>
2016-09-29 20:41:42 +0300

    fix+

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,10 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
-  before_action :set_question, only: [:create, :destroy]
   before_action :find_answer, only: [:destroy]
   before_action :must_be_author!, only: [:destroy]
 
   def create
+    @question = Question.find(params[:question_id])  
     @answer = @question.answers.new(answer_params)
     @answer.user = current_user
     if @answer.save
@@ -16,19 +16,15 @@ class AnswersController < ApplicationController
 
   def destroy
     @answer.destroy
-    redirect_to @question
+    redirect_to question_path(@answer.question)
   end
 
   private
 
   def must_be_author!
     unless current_user.author_of?(@answer)
-      redirect_to @question, error: "You can delete only your answer"
+      redirect_to @answer.question, error: "You can delete only your answer"
     end
-  end
-
-  def set_question
-    @question = Question.find(params[:question_id])  
   end
 
   def answer_params
@@ -36,6 +32,6 @@ class AnswersController < ApplicationController
   end
 
   def find_answer
-    @answer = @question.answers.find(params[:id])
+    @answer = Answer.find(params[:id])
   end
 end
