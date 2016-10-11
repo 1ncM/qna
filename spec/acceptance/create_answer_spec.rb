d01bf9c02e487cca8d08c2213873eeeb29b1f1cb
Alexandr <microposts@gmail.com>
2016-10-11 22:48:46 +0300

    finish

--- spec/acceptance/create_answer_spec.rb
+++ spec/acceptance/create_answer_spec.rb
@@ -6,11 +6,10 @@ feature 'Write answer for question', %{
   I want to be able to write answer for question
 } do
 
-  given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  scenario 'Authenticated user write answer for question' do
-    sign_in(user)
+  scenario 'Authenticated user write answer for question', js: true do
+    sign_in create(:user)
     visit question_path(question)
     fill_in 'answer_body', with: 'Text text'
     click_on 'Post Your Answer'
@@ -18,6 +17,15 @@ feature 'Write answer for question', %{
     within '.answers' do
       expect(page).to have_content('Text text')
     end
+  end
+
+  scenario 'Authenticated user submit empty answer for the question', js: true do
+    sign_in create(:user)
+    visit question_path(question)
+    fill_in 'answer_body', with: ''
+    click_on 'Post Your Answer'
+    expect(current_path).to eq question_path(question)
+    expect(question.answers.count).to eq 0
   end
 
   scenario 'Non-authenticated user can not create answer on the question' do
